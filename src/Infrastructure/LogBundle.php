554933bf0d4b655fdbe2b994be023f434b580729
vitaliy <zndev@ya.ru>
2023-04-17 18:30:36 +0600

    upd

--- src/Infrastructure/LogBundle.php
+++ src/Infrastructure/LogBundle.php
@@ -2,6 +2,7 @@
 
 namespace Untek\Utility\Log\Infrastructure;
 
+use Untek\Core\Code\Helpers\DeprecateHelper;
 use Untek\Core\Kernel\Bundle\BaseBundle;
 
 class LogBundle extends BaseBundle
@@ -10,10 +11,5 @@ class LogBundle extends BaseBundle
     public function getName(): string
     {
         return 'log';
-    }
-
-    public function boot(): void
-    {
-        $this->configureFromPhpFile(__DIR__ . '/../Domain/config/container.php');
     }
 }
